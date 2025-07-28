22befb0e753c27590aed0b62ba12623d0c4adac8
RoseAstral <lilianascarletaurora@gmail.com>
2025-07-28 14:28:59 +0200

    Added Autumn advice (close #1)

--- garden_advice.py
+++ garden_advice.py
@@ -1,19 +1,27 @@
 # Hardcoded values for the season and plant type
-season = "summer"  # TODO: Replace with input() to allow user interaction.
-plant_type = "flower"  # TODO: Replace with input() to allow user interaction.
+season = input("plase enter 'summer' or 'winter': ")
+plant_type = input("Please enter 'flower' or 'vegetable': ")
 
 # Variable to hold gardening advice
 advice = ""
 
 # Determine advice based on the season
+'''
+This block of code determines what season the user has chosen and adds the string to the "advice" varible.
+'''
 if season == "summer":
     advice += "Water your plants regularly and provide some shade.\n"
 elif season == "winter":
     advice += "Protect your plants from frost with covers.\n"
+elif season == 'autumn':
+    advice += 'This a great growing type for fruits and vegetables make sure they recive the nutrients they need\n'
 else:
     advice += "No advice for this season.\n"
 
 # Determine advice based on the plant type
+'''
+This block of code determines what type of plant the user has chosen and adds the string to the "advice" varible.
+'''
 if plant_type == "flower":
     advice += "Use fertiliser to encourage blooms."
 elif plant_type == "vegetable":
